d67c025e21ac3fb6ac4109854db4cf3b5c7ba005
Omar Faruque Tuhin <nadimtuhin@gmail.com>
2024-08-09 03:29:16 +0600

    feat: added fork button

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,26 +1,32 @@
-import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
 import './globals.css';
 
 export default function Layout({children}) {
   return (
-    <html>
+    <html lang="en">
     <Head>
       <title>Interim Government Cabinet</title>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
     </Head>
     <body>
     <div className="flex flex-col min-h-screen">
       <header className="bg-gray-800 text-white py-4">
         <nav className="container mx-auto flex justify-between items-center">
           <Link href="/" className="text-xl font-bold">
-            <span>Interim Government Cabinet</span>
+              Interim Government Cabinet
           </Link>
           <ul className="flex space-x-4">
             <li>
               <Link href="/">
-                <span>Home</span>
+                Home
               </Link>
+            </li>
+            <li>
+              <a href="https://github.com/nadimtuhin/bangladesh-interim-govt" className="flex items-center space-x-2">
+                <i className="fab fa-github"></i>
+                <span>Fork on GitHub</span>
+              </a>
             </li>
           </ul>
         </nav>
